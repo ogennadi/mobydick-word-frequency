54c0c686b864a80f8baecb1b1e4bf552b71cf0f9
Oge Nnadi <oge@nnadi.org>
2019-05-17 21:28:26 -0500

    Initial commit

---
+++ spike.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(stringr)
+library(tidytext)
+
+STOP_WORDS_FILE <- 'stop-words.txt'
+MOBYDICK_FILE <- 'mobydick.txt'
+JOIN_COLUMN <- 'word'
+
+data(stop_words)
+
+sa_stop_words <- read.table(STOP_WORDS_FILE, as.is=FALSE, col.names = c(JOIN_COLUMN), colClasses = 'character') %>% 
+  tibble(word = .$word)
+
+sa_mobydick <- read.table(MOBYDICK_FILE, as.is=FALSE,  col.names = 'text', colClasses = 'character', sep = '\n', row.names = NULL) %>% 
+  tibble(text = .$text) %>%
+  unnest_tokens(word, text)
+
+sa_moby_count <- sa_mobydick %>%
+  anti_join(sa_stop_words, by = JOIN_COLUMN) %>%
+  count(word, sort=TRUE)
+
+sa_moby_count %>% print(n=100)
