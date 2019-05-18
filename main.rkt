ec069b7a453cfc14c3b65ebab960b8922bedd993
Oge Nnadi <oge@nnadi.org>
2019-05-18 00:33:49 -0500

    Read list of stop words

--- main.rkt
+++ main.rkt
@@ -6,5 +6,17 @@
 
 (define (calculate-word-freq)
   WORD-FREQ)
-
 (check-equal? (calculate-word-freq) WORD-FREQ)
+
+(define STOP-WORDS
+  (let ([not-stop-word? (lambda (x) (or (equal? x "") (string-prefix? x "#")))])
+    (filter-not  not-stop-word? (file->lines "stop-words.txt"))))
+(check-equal? (length STOP-WORDS) 429)
+
+
+;; Wishlist:
+;; MOBY_WORDS
+;; (bagify lst)  (https://stackoverflow.com/questions/5740307/count-occurrence-of-element-in-a-list-in-scheme#5740464)
+;; (remv* v-lst lst)
+;; (bagify (remv* STOP-WORDS MOBY-WORDS))
+
