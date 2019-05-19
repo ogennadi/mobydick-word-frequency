69d63f6b360392bad442df2e673d2bb500366b10
Oge Nnadi <oge@nnadi.org>
2019-05-19 10:37:24 -0500

    Move tests into their own module

--- lib.rkt
+++ lib.rkt
@@ -1,15 +1,9 @@
 #lang racket
-(require 2htdp/batch-io
-         rackunit)
-
-(provide RACKET-WORD-FREQ)
+(provide RACKET-WORD-FREQ STOP-WORDS MOBY-WORDS count-frequency)
 
 (define STOP-WORDS
   (let ([not-stop-word? (lambda (x) (or (equal? x "") (string-prefix? x "#")))])
     (filter-not  not-stop-word? (file->lines "stop-words.txt"))))
-(check-equal? (length STOP-WORDS) 429)
-
-;;;
 
 (define (expand-number-acronym str)
   (if (string-prefix? str "1.")
@@ -22,34 +16,15 @@
          [lowercase-trimmed (map (compose string-downcase trim) rough-tokens)]
          [smooth-tokens (flatten (map expand-number-acronym lowercase-trimmed))])
     (remove* '("#2701" "") smooth-tokens)))
-(define R-MOBY-WORDS
-  (file->lines "r-words.txt"))
 
-(check-equal? (length MOBY-WORDS) (length R-MOBY-WORDS))
-
-(display-lines-to-file MOBY-WORDS "racket-words.txt" #:exists 'replace)
-
-;;;
-
+;; https://stackoverflow.com/a/5741004/2042190
 (define (count-frequency lst)
   (foldl (lambda (key ht)
            (hash-update ht key add1 0))
          #hash() lst))
-(check-equal? (count-frequency '()) '#hash())
-(check-equal? (count-frequency '("a" "a" "t")) '#hash(("a" . 2) ("t" . 1)))
-
-;;;
-
-(define R-WORD-FREQ
-  (let* ([raw-data (rest (read-csv-file "word-frequency.csv"))]
-         [->string-and-number (lambda (lst)
-                                (list (first lst) (string->number (second lst))))])
-    (map ->string-and-number raw-data)))
 
 (define RACKET-WORD-FREQ
   (let* ([freq-hash   (count-frequency (remove* STOP-WORDS MOBY-WORDS))]
          [freq-list   (hash-map freq-hash list)]
          [sorted-list (sort freq-list > #:key last)])
     (take sorted-list 100)))
-
-(check-true (set=? RACKET-WORD-FREQ R-WORD-FREQ))
---
+++ test.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require "lib.rkt"
+         2htdp/batch-io
+         rackunit)
+
+(check-equal? (length STOP-WORDS) 429)
+
+;;;
+
+(define R-MOBY-WORDS
+  (file->lines "r-words.txt"))
+
+(check-equal? (length MOBY-WORDS) (length R-MOBY-WORDS))
+
+(display-lines-to-file MOBY-WORDS "racket-words.txt" #:exists 'replace)
+
+;;;
+
+(check-equal? (count-frequency '()) '#hash())
+(check-equal? (count-frequency '("a" "a" "t")) '#hash(("a" . 2) ("t" . 1)))
+
+;;;
+
+(define R-WORD-FREQ
+  (let* ([raw-data (rest (read-csv-file "word-frequency.csv"))]
+         [->string-and-number (lambda (lst)
+                                (list (first lst) (string->number (second lst))))])
+    (map ->string-and-number raw-data)))
+
+(check-true (set=? RACKET-WORD-FREQ R-WORD-FREQ))
